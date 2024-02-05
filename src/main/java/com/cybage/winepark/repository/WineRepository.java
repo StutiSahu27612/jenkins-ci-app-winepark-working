f3077480713a94a83eae61782a334b885f93b395
StutiSahu27612 <112114710+StutiSahu27612@users.noreply.github.com>
2024-02-05 16:36:05 +0530

    Update WineRepository.java

--- src/main/java/com/cybage/winepark/repository/WineRepository.java
+++ src/main/java/com/cybage/winepark/repository/WineRepository.java
@@ -4,7 +4,7 @@ import com.cybage.winepark.domain.Wine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository 
 public interface WineRepository extends JpaRepository<Wine, Integer> {
     Wine findByWineId(Integer wineId);
 }
