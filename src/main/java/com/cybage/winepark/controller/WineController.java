892e5f1f46d3f975bfb13777ebe5dd7a8624a756
StutiSahu27612 <112114710+StutiSahu27612@users.noreply.github.com>
2024-02-05 16:41:12 +0530

    Update WineController.java

--- src/main/java/com/cybage/winepark/controller/WineController.java
+++ src/main/java/com/cybage/winepark/controller/WineController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpHeaders; 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*; 
